21ce84fb3bce38e5f967a2c85dd93a303b048552
bodoh69 <bodoh@69>
2022-03-23 18:55:32 -0500

    Default to opening seeprom.bin and otp.bin if they exist.
    Add check for -h as first argument and show usage.
    Add .gitignore file.

--- wiiu_seeprom_parser.c
+++ wiiu_seeprom_parser.c
@@ -23,34 +23,50 @@ int use_otp = 0;
 int main (int argc, char *argv[])
 {
 	printf("\n=======================================\n");
-	printf("   Wii U SEEPROM parser v1   \n");
+	printf("   Wii U SEEPROM parser v1.1   \n");
 	printf("=======================================\n\n");
 
 	printf("Based on http://wiiubrew.org/wiki/Hardware/OTP\n\n");
-	if (argc < 2) {
-		printf("USAGE: seeprom_parse seeprom.bin [otp.bin]\n\n");
-		return -1;
-	}
-	
 	//! Run program
 	//Initialize variables
 	memset((void *)&seeprom,      0, sizeof(seeprom_t));
 	memset((void *)&boot_param1,  0, sizeof(boot_param1_t));
 	memset((void *)&boot_param2,  0, sizeof(boot_param2_t));
 	memset((void *)&boot_param3,  0, sizeof(boot_param3_t));
-		
-	if (argc < 3)
-		printf("WARNING: no otp.bin specified, some values won't be decrypted\n\n");
-	else {
-		//Load otp file
-		printf("Reading otp...\n");
-		FILE * otp_file;
+
+	FILE * otp_file;
+	FILE * seeprom_file;
+	if (argc < 2) {
+		seeprom_file = fopen("seeprom.bin", "rb");
+	} else {
+		if (strncmp(argv[1], "-help", 2) == 0) {
+			printf("USAGE: seeprom_parse seeprom.bin [otp.bin]\n\n");
+			return 0;
+		}
+		seeprom_file = fopen(argv[1], "rb");
+	}
+	if (seeprom_file == NULL) {
+		if (argc < 2) {
+			printf("USAGE: seeprom_parse seeprom.bin [otp.bin]\n\n");
+			return -1;
+		}
+		printf("ERROR: failed to open %s!\n", argc < 2 ? "seeprom.bin" : argv[1]);
+		return -1;
+	}
+
+	if (argc > 2) {
 		otp_file = fopen(argv[2], "rb");
 		if (otp_file == NULL) {
 			printf("ERROR: failed to open %s!\n", argv[2]);
 			return -1;
 		}
-		if (fread(otp, 1, 0x400, otp_file) != 0x400) {
+	} else {
+		otp_file = fopen("otp.bin", "rb");
+	}
+	if (otp_file != NULL) {
+		//Load otp file
+		printf("Reading otp...\n");
+		if (fread(otp, 1, 0x400, otp_file) != 0x400 && argc > 2) {
 			printf("ERROR: Failed to read %s!\n", argv[2]);
 			return -1;
 		}
@@ -58,30 +74,24 @@ int main (int argc, char *argv[])
 		printf("Successfully read otp.\n");
 		use_otp = 1;
 	}
-	
+
 	//Load seeprom file
-    printf("Reading seeprom...\n");
-	FILE * seeprom_file;
-	seeprom_file = fopen(argv[1], "rb");
-    if (seeprom_file == NULL) {
-        printf("ERROR: failed to open %s!\n", argv[1]);
-		return -1;
-    }
+	printf("Reading seeprom...\n");
 	if (fread (&seeprom, 1, sizeof(seeprom_t), seeprom_file) != 512) {
 		printf("ERROR: Failed to read %s!\n", argv[1]);
 		return -1;
 	}
 	fclose(seeprom_file);
 	printf("Successfully read seeprom.\n\n\n\n\n\n\n");
-	
+
 	// Print SEEPROM values
 	printf("================SEEPROM VALUES================\n\n\n");
-	
+
 	print_hex("SEEPROM PRNG seed:...............", seeprom.seeprom_prng_seed, 0x08);
 	print_hex("PPC PVR (should be 0x70010201):..", seeprom.ppc_pvr, 0x04);
 	printf   ("ASCII Tag 1:.....................\"%.*s\"\n", 0x6, seeprom.ascii_tag1);
 	printf   ("OTP tag:.........................\"%.*s\"\n", 0x8, seeprom.otp_tag);
-	
+
 	//Print BC struct
 	printf   ("\n-----------------Board Config----------------\n");
 	print_hex("CRC32:...........................", seeprom.bc_struct.crc32, 0x4);
@@ -103,30 +113,30 @@ int main (int argc, char *argv[])
 	print_hex("consoleType:.....................", seeprom.bc_struct.consoletype, 0x2);
 	print_hex("devicePresence:..................", seeprom.bc_struct.devicepresence, 0x4);
 	printf   ("---------------------------------------------\n\n");
-	
+
 	//Keys
 	print_hex("Wii U drive key:.................", seeprom.wii_u_drive_key, 0x10);
 	print_hex("Wii U factory key:...............", seeprom.wii_u_factory_key, 0x10);
 	print_hex("Wii U devkit key (?):............", seeprom.wii_u_devkit_key, 0x10);
 	print_hex("Wii U USB key seed:..............", seeprom.wii_u_usb_key_seed, 0x10);
-	
+
 	//Keys states
 	printf   ("Drive key's status flag:.........");
 	u16 dksf = seeprom.drive_key_status_flag[0] << 8 | seeprom.drive_key_status_flag[1]; //drive key status flag
 	if (dksf == 0xFFFF)       printf("SET, encrypted with Wii U SEEPROM key (0xFFFF)\n");
 	else if (dksf == 0x0000)  printf("SET, not encrypted (0x0000)\n");
-	
+
 	printf   ("USB key seed's status flag:......");
 	u16 ukssf = seeprom.usb_key_seed_status_flag[0] << 8 | seeprom.usb_key_seed_status_flag[1]; //drive key status flag
 	if (ukssf == 0xFFFF)      printf("SET, encrypted with Wii U SEEPROM key (0xFFFF)\n");
 	else if (ukssf == 0x0000) printf("SET, not encrypted (0x0000)\n");
-	
+
 	printf   ("Devkit key's status flag:........");
 	u16 dkksf = seeprom.devkit_key_status_flag[0] << 8 | seeprom.devkit_key_status_flag[1]; //drive key status flag
 	if (dkksf == 0xFFFF)      printf("SET, encrypted with a key from OTP (0xFFFF) [whoa, you're using a devkit??]\n");
 	else if (dkksf == 0x0000) printf("NOT SET (0x0000)\n");
-	
-	
+
+
 	// SLC: sys_prod
 	printf   ("\n-----------------SLC:sys_prod----------------\n");
 	print_hex("version:.........................", seeprom.sys_prod.version, 0x4);
@@ -140,41 +150,41 @@ int main (int argc, char *argv[])
 	printf   ("serial_id:.......................\"%.*s\"\n", 0x10, seeprom.sys_prod.serial_id);
 	printf   ("model_number:....................\"%.*s\"\n", 0x10, seeprom.sys_prod.model_number);
 	printf   ("---------------------------------------------\n\n");
-	
+
 	// Other values
 	printf   ("ASCII Tag 2:.....................\"%.*s\"\n", 0x8, seeprom.ascii_tag2);
-	
-	
+
+
 	// Decrypt boot parameters
 	if (use_otp == 1) {
 		//Take Wii U seeprom key from OTP
 		u8 wiiu_seeprom_key[0x10];
 		memcpy(wiiu_seeprom_key, &otp[0x0A0], 0x10);
-		
-		print_hex("\n\nDecrypting boot prameters with key ", wiiu_seeprom_key, 0x10);
-		
+
+		print_hex("\n\nDecrypting boot parameters with key ", wiiu_seeprom_key, 0x10);
+
 		//IV for all the parameters
 		u8 boot_param_IV[0x10];
 		memset(boot_param_IV, 0x0, 0x10); // IV = 0 (?)
-		
+
 		//Decrypt boot flags
 		AES128_CBC_decrypt_buffer((u8*)&boot_param1, seeprom.boot_param1_enc, 0x10, wiiu_seeprom_key, boot_param_IV);
 		AES128_CBC_decrypt_buffer((u8*)&boot_param2, seeprom.boot_param2_enc, 0x10, wiiu_seeprom_key, boot_param_IV);
 		AES128_CBC_decrypt_buffer((u8*)&boot_param3, seeprom.boot_param3_enc, 0x10, wiiu_seeprom_key, boot_param_IV);
-		
-		printf   ("Boot parameres:\n");		
-		
+
+		printf   ("Boot parameters:\n");
+
 		u16 control_flag = boot_param1.control_flags[0] << 8 | boot_param1.control_flags[1];
 		// TODO: Bits counted from more important bits or less important bit??
 		u16 cpu_speed =					(control_flag & 0xFFC0) >> 6; // 1111111111000000
 		u16 io_delay_before_sd_check =	(control_flag & 0x003E) >> 1; // 0000000000111110
 		u16 clock_increased =			(control_flag & 0x0001); // 0000000000000001
-		
+
 		u16 nand_cotrol_flags = boot_param1.nand_cotrol_flags[0] << 8 | boot_param1.nand_cotrol_flags[1];
 		// TODO: Bits counted from more important bits or less important bit??
 		u16 overwrite_NAND_BANK   =	(nand_cotrol_flags & 0x0004) >> 2; // 0000000000000100
 		u16 overwrite_NAND_CONFIG =	(nand_cotrol_flags & 0x0002) >> 1; // 0000000000000010
-		
+
 		printf   ("    Control flags (clock config + SMC delay):\n");
 		printf   ("        CPU speed in MHz:......................0x%04X\n", cpu_speed);
 		printf   ("        IO delay before checking SD boot:......0x%04X\n", io_delay_before_sd_check);
@@ -186,18 +196,18 @@ int main (int argc, char *argv[])
 		print_hex("    Overwrite NAND_BANK with value:............", boot_param1.nand_bank_overwrite, 0x4);
 		print_hex("    CRC32:.....................................", boot_param1.crc32, 0x4);
 
-		printf   ("Boot parameres for boot0 about boot1:\n");
+		printf   ("Boot parameters for boot0 about boot1:\n");
 		print_hex("    boot1 version:.............................", boot_param2.boot1_version, 0x2);
 		print_hex("    boot1 NAND sector:.........................", boot_param2.boot1_nand_sector, 0x2);
 		print_hex("    CRC32:.....................................", boot_param2.crc32, 0x4);
-		
-		printf   ("Boot parameres for boot0 about boot1 copy:\n");
+
+		printf   ("Boot parameters for boot0 about boot1 copy:\n");
 		print_hex("    boot1 copy version:........................", boot_param3.boot1_copy_version, 0x2);
 		print_hex("    boot1 copy NAND sector:....................", boot_param3.boot1_copy_nand_sector, 0x2);
 		print_hex("    CRC32:.....................................", boot_param3.crc32, 0x4);
 	}
-	
+
 	printf("\n\n=============================================\n\n");
-	
+
     return 0;
 }
